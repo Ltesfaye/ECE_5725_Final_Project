501af1798e9c4defe438a6a18dacbd7c5d956899
lst26 <lst26@cornell.edu>
2020-05-03 19:05:06 -0400

    setting up connection

---
+++ test_connection.py
@@ -0,0 +1,29 @@
+import bluetooth 
+import json
+import time
+import subprocess
+import os
+from SpectralData import SpectralData
+
+# Global Variables
+srcPath = "../data-collector/build/data-collector"
+dirPath = os.path.dirname(os.path.realpath(__file__))
+spectralData = SpectralData()
+sendString = ""
+
+while True:
+    # Define Server Socket on RFCOMM
+    serverSocket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    serverSocket.bind(("", bluetooth.PORT_ANY))
+    # Listen on port 1
+    port = serverSocket.getsockname()[1]
+    serverSocket.listen(1)
+    print("listening on port", port)
+
+    # Advertise Socket
+    uuid = "2f3b0104-fcb0-4bcf-8dda-6b06390c3c1a"
+    bluetooth.advertise_service(serverSocket, "spectral-data-collector", uuid)
+
+    # Wait for client to connect
+    clientSocket, address = serverSocket.accept()
+    print("Accepted connection from ", address)
