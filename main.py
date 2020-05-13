efdc2caf8f662035ef8323dad70c8c8fc738babd
Leul Tesfaye <Leul Tesfaye>
2020-05-13 19:01:31 -0400

    testing5

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import bluetooth
-from threading import Thread
 
+#Setting up the debug flag
 debug = False
+
+#setting up bluetooth server socket
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
+#waiting for a connection on port1
 port=1
 server_sock.bind(("",port))
 server_sock.listen(1)
@@ -13,24 +16,19 @@ client_sock,address = server_sock.accept()
 print ("Accepted connection from", address)
 if debug:
     while True:
-        
-    
         data = client_sock.recv(1024)
-
         print ("received [%s]" %data)
 
         if (data=="e"):
             print("Exit")
             break
 else:
-    from animate_phone import Animate_phone
     print("____Starting Display____")
+    from animate_phone import Animate_phone
     display = Animate_phone(client_sock,server_sock)
-
     display.run()
 
 
     
-
 client_sock.close()
 server_sock.close()
