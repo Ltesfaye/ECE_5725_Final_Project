e80b7e76f3096276501a1f25036c766446cfb072
Leul Tesfaye <Leul Tesfaye>
2020-05-14 18:27:49 -0400

    testing stuff 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import bluetooth
 import signal
 import sys
+import time
 
 
 
@@ -18,12 +19,12 @@ server_sock.bind(("",port))
 server_sock.listen(1)
 
 client_sock,address = server_sock.accept()
-
-client_sock,address = server_sock.accept()
 print ("Resetting connection from", address)
 
 client_sock.close()
 server_sock.close()
+
+time.sleep(1)
 
 
 #setting up bluetooth server socket
@@ -33,7 +34,7 @@ server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 port=1
 server_sock.bind(("",port))
 server_sock.listen(1)
-
+client_sock,address = server_sock.accept()
 
 print ("Accepted connection from", address)
 
