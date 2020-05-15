bbb778b2b9ac45e51de6fb67eeb961ab9a970101
Leul Tesfaye <Leul Tesfaye>
2020-05-15 04:17:42 -0400

    will have to do

--- testing/display_testing.py
+++ testing/display_testing.py
@@ -1,12 +1,14 @@
 from bluedot.btcomm import BluetoothServer
-from signal import pause
+# from signal import pause
 
 def data_received(data):
     print(data)
     
 
 s = BluetoothServer(data_received)
-pause()
+while True:
+    
+# pause()
 
 
 
