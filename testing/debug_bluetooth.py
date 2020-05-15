40c9230786531913918b7914fe343f1b5a27cf30
Leul Tesfaye <Leul Tesfaye>
2020-05-15 02:23:12 -0400

    last minute

--- testing/debug_bluetooth.py
+++ testing/debug_bluetooth.py
@@ -2,13 +2,33 @@ import bluetooth
 import time
 
 
+
+#setting up bluetooth server socket
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
+#waiting for a connection on port1
 port=1
 server_sock.bind(("",port))
 server_sock.listen(1)
 
 client_sock,address = server_sock.accept()
+print ("Resetting connection from", address)
+
+client_sock.close()
+server_sock.close()
+
+time.sleep(1)
+
+#setting up bluetooth server socket
+server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+#waiting for a connection on port1
+port=1
+server_sock.bind(("",port))
+server_sock.listen(1)
+client_sock,address = server_sock.accept()
+
+client_sock.settimeout(1)
 
 print ("Accepted connection from", address)
 start = time.time()
